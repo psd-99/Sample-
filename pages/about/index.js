86ba4988c0a28cafb20538f5fd52b486ec0566b8
psd-99 <pasinduekanayaka99@gmail.com>
2024-07-05 23:31:47 +0530

    modified some code for about page

--- pages/about/index.js
+++ pages/about/index.js
@@ -53,40 +53,7 @@ export const aboutData = [
       },
     ],
   },
-  // {
-  //   title: 'experience',
-  //   info: [
-  //     {
-  //       title: 'UX/UI Designer - XYZ Company',
-  //       stage: '2012 - 2023',
-  //     },
-  //     {
-  //       title: 'Web Developer - ABC Agency',
-  //       stage: '2010 - 2012',
-  //     },
-  //     {
-  //       title: 'Intern - DEF Corporation',
-  //       stage: '2008 - 2010',
-  //     },
-  //   ],
-  // },
-  // {
-  //   title: 'credentials',
-  //   info: [
-  //     {
-  //       title: 'Web Development - ABC University, LA, CA',
-  //       stage: '2011',
-  //     },
-  //     {
-  //       title: 'Computer Science Diploma - AV Technical Institute',
-  //       stage: '2009',
-  //     },
-  //     {
-  //       title: 'Certified Graphic Designer - ABC Institute, Los Angeles, CA',
-  //       stage: '2006',
-  //     },
-  //   ],
-  // },
+  
 ];
 
 // components
